22dd740ee54f2a0dfa4c43394369758d32ad222c
VolodymyrBab <volbv@yahoo.com>
2023-04-18 23:25:39 +0200

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,33 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryRef = document.querySelector('.gallery');
+// console.log(galleryRef);
+
+
+const makeGalleryMarkup = galleryItem => {
+  return `<li class="gallery__item">
+  <a  href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}" 
+    alt="${galleryItem.description}" />
+  </a>
+  </li>`;
+};
+
+const makeGallery = galleryItems.map(makeGalleryMarkup).join('');
+galleryRef.insertAdjacentHTML('afterbegin', makeGallery);
+
+
+const gallery = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: '250',
+  scrollZoom: false,
+});
