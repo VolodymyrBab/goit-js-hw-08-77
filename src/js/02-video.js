c86512ba848c028b0c174050ed760b78386dc795
VolodymyrBab <volbv@yahoo.com>
2023-04-19 00:12:29 +0200

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+// console.log(iframe);
+const throttle = require('lodash.throttle');
+const player = new Player(iframe);
+// console.log(player);
+
+player.on('timeupdate', throttle(currentTimeWatching, 1000));
+
+function currentTimeWatching(evt) {
+  localStorage.setItem('videoplayer-current-time', evt.seconds);
+};
+
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
